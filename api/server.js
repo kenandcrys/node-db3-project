a8a507a1640e4bb7870faef166a21de61b8a27e8
Kenneth Fisher <kenandcrys@live.com>
2023-10-02 00:25:13 -0500

    Set up File structure

--- api/server.js
+++ api/server.js
@@ -1,10 +1,21 @@
 const express = require('express');
 
-const SchemeRouter = require('./schemes/scheme-router.js');
-
+const CattleRouter = require('./Cattle/Routers/cattle-router.js');
+const EquineRouter = require('./Cattle/Routers/equine-router.js');
+const SwineRouter = require( './Cattle/Routers/swine-router.js');
+const SheepAndGoatsRouter = require('./Cattle/Routers/SheepAndGoats-router.js');
+const StraigtGrainsRouter = require('./Cattle/Routers//StraightGrains-router.js');
+const PoultryRouter = require('./Cattle//Routers/Poultry-router.js');
+const RabbitRouter = require('./Cattle/Routers/Rabbit-router.js');
 const server = express();
 
 server.use(express.json());
-server.use('/api/schemes', SchemeRouter);
+server.use('/api/Cattle', CattleRouter);
+server.use('/api/equine', EquineRouter);
+server.use('/api/swine',   SwineRouter);
+server.use('/api/rabbit', RabbitRouter);
+server.use('/api/poultry',PoultryRouter);
+server.use('/api/sheepandgoat', SheepAndGoatsRouter);
+server.use('/api/straightGrain', StraigtGrainsRouter);
 
 module.exports = server;
